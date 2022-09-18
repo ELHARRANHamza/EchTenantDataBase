67d58215e27304c5f6403dfb63369a289e73d5a8
hamza <harranons@gmail.com>
2022-09-18 16:46:47 +0100

    each tenant has database

---
+++ src/sample.Application/TenantConnectionResolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Volo.Abp.Data;
+using Volo.Abp.MultiTenancy;
+
+namespace sample
+{
+    public class TenantConnectionResolver: DefaultConnectionStringResolver
+    {
+        private readonly ICurrentTenant _currentTenant;
+
+        private readonly IServiceProvider _serviceProvider;
+        public TenantConnectionResolver(IOptionsMonitor<AbpDbConnectionOptions> options, ICurrentTenant currentTenant,IServiceProvider serviceProvider) :base(options)
+        {
+            _currentTenant = currentTenant;
+            _serviceProvider = serviceProvider;
+        }
+        public override async Task<string> ResolveAsync(string connectionStringName = null)
+        {
+            if (_currentTenant.Id.HasValue && connectionStringName == "Default")
+            {
+                var find = FindTenantConfiguration(_currentTenant.Id.GetValueOrDefault());
+                var conection = find.ConnectionStrings.Values.FirstOrDefault();
+                return conection;
+            }
+            return await base.ResolveAsync(connectionStringName);
+        }
+        public override  string Resolve(string connectionStringName = null)
+        {
+            if (_currentTenant.Id.HasValue && connectionStringName == "Default")
+            {
+                var find = FindTenantConfiguration(_currentTenant.Id.GetValueOrDefault());
+                var conection = find.ConnectionStrings.Values.FirstOrDefault();
+                return conection;
+            }
+            return  base.Resolve(connectionStringName);
+        }
+
+
+        protected TenantConfiguration FindTenantConfiguration(Guid tenantId)
+        {
+            using IServiceScope serviceScope = _serviceProvider.CreateScope();
+           
+            return serviceScope.ServiceProvider.GetRequiredService<ITenantStore>().Find(tenantId);
+        }
+    }
+}
+
